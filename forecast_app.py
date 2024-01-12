23509da6715bba2f888f806c678348c2d677b3f4
krisnaAditya <120473425+artesia0528@users.noreply.github.com>
2024-01-12 12:51:33 +0800

    Update forecast_app.py
    update plot raw data

--- forecast_app.py
+++ forecast_app.py
@@ -42,9 +42,8 @@ st.write(data.tail(30))
 # Plot raw data
 def plot_raw_data():
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+    fig.layout.update(title_text='Time Series data', xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
 plot_raw_data()
